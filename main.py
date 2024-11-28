57859f7de9dc00495d980f61652870a3ea357a91
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-11-27 19:37:01 -0600

    Added terminal interface

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from proveedor import Proveedor
 from producto import Producto
-from data import insert_producto
+from data import insert_producto, update_producto
 
-productos = []
+print("-- LIGHT RETAIL MANAGEMENT --")
+primera_ejecucion = int(input("Es esta la primera ejecucion (1 > Si / 2 > No)? "))
 
-proveedor = Proveedor("DISTELSA", "42828238")
+while primera_ejecucion == 1:
 
-producto = Producto("Samsung", 6, 2500, 3100, proveedor)
-productos.append({
-    'id': producto.id, 
-    'nombre': producto.nombre, 
-    'cantidad': producto.cantidad, 
-    'precio_entrada': producto.precio_entrada, 
-    'precio_salida': producto.precio_salida, 
-    'proveedor': producto.proveedor.nombre})
+    continuar = 1
+    
+    print("-- Ingrese proveedor del producto --")
+    nombre_proveedor = input("Proveedor: ")
+    telefono_proveedor= input("Número: ")
+    proveedor = Proveedor(nombre_proveedor, telefono_proveedor)
+    productos = []
 
+    while continuar == 1:
+        
+        print("-- Ingrese datos del producto --")
+        nombre_producto = input("Nombre: ")
+        cantidad = input("Cantidad: ")
+        precio_entrada = input("Precio de compra: ")
+        precio_salida = input("Precio de venta: ")
 
-producto = Producto("LG", 6, 2250, 2700, proveedor)
-productos.append({
-    'id': producto.id, 
-    'nombre': producto.nombre, 
-    'cantidad': producto.cantidad, 
-    'precio_entrada': producto.precio_entrada, 
-    'precio_salida': producto.precio_salida, 
-    'proveedor': producto.proveedor.nombre})
+        producto = Producto(nombre_producto, cantidad, precio_entrada, precio_salida, proveedor)
+        productos.append({
+            'id': producto.id, 
+            'nombre': producto.nombre, 
+            'cantidad': producto.cantidad, 
+            'precio_entrada': producto.precio_entrada, 
+            'precio_salida': producto.precio_salida, 
+            'proveedor': producto.proveedor.nombre})
 
-insert_producto(productos)
-print(productos)
+        #print(productos)
+        continuar = int(input("Desea agregar otro producto del mismo proveedor (1 > Si / 2 > No)? "))
+    
+    insert_producto(productos)
+    primera_ejecucion = 2
+
 
 
