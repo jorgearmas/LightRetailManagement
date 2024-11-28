5e4ea8fbdb3b6a7f3808ffeddefb847dcdf35d0a
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-11-27 22:23:12 -0600

    implemented func to read cvs and load it to list of dictionaries

--- data.py
+++ data.py
@@ -11,3 +11,14 @@ def insert_producto(productos):
 
         for fila in productos:
             producto_writer.writerow(fila)
+
+def read_producto():
+    productos = []
+    with open('productos.csv', newline='') as productos_csv:
+        producto_reader = csv.DictReader(productos_csv, delimiter=',')
+        for lines in producto_reader:
+            productos.append(lines)
+    
+    return productos
+
+
