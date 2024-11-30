2f8bd6e0d33509c6a3d79fbc3a6cb62c7c54193e
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-11-30 12:52:43 -0600

    function exec_carga_no_existente completed

--- carga.py
+++ carga.py
@@ -83,8 +83,12 @@ def exec_carga_stock():
     #instanciar 'Producto' con productos existentes del diccionario 'productos'
     instanciar_productos(productos)
 
+    #input id a buscar
     id_to_search = input("- Ingrese ID a buscar: ")
 
+    #1. iterar sobre lista de productos
+    #2. iterar sobre registro encontrar id a modificar
+    #3. modificar cantidad (stock)
     for registro in productos:
         for key, value in registro.items():
             if key == 'id' and value == id_to_search:
@@ -96,5 +100,35 @@ def exec_carga_stock():
     
     insert_producto(productos)
             
-        
+def exec_carga_no_existente():
 
+    #instanciar 'Producto' con productos existentes del diccionario 'productos'
+    instanciar_productos(productos)
+
+    continuar = 1
+    
+    print("-- Ingrese proveedor del producto --")
+    nombre_proveedor = input("Proveedor: ")
+    proveedor = Proveedor(nombre_proveedor)
+
+    while continuar == 1:
+        
+        print("-- Ingrese datos del producto --")
+        nombre_producto = input("Nombre: ")
+        cantidad = input("Cantidad: ")
+        precio_entrada = input("Precio de compra: ")
+        precio_salida = input("Precio de venta: ")
+
+        producto = Producto(nombre_producto, cantidad, precio_entrada, precio_salida, proveedor)
+        productos.append({
+            'id': producto.id, 
+            'nombre': producto.nombre, 
+            'cantidad': producto.cantidad, 
+            'precio_entrada': producto.precio_entrada, 
+            'precio_salida': producto.precio_salida, 
+            'proveedor': producto.proveedor.nombre})
+
+        #print(productos)
+        continuar = int(input("Desea agregar otro producto del mismo proveedor (1 > Si / 2 > No)? "))
+    
+    insert_producto(productos)
