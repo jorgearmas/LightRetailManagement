5c307a45a347a09a670faa1a87f2d6bd25500726
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-03 16:01:58 -0600

    Added function to remove items from invetory

--- descarga.py
+++ descarga.py
@@ -6,7 +6,16 @@ productos = read_producto()
 
 def exec_descarga():
 
-    id_to_search = input("- Ingrese ID a buscar: ")
-    for registro in productos:
-        for key, value in registro.items():
-            print(registro)
+    continuar = 1
+    while continuar == 1:
+        id_to_search = input("- Ingrese ID a buscar: ")
+        for registro in productos:
+            for key, value in registro.items():
+                if key == 'id' and value == id_to_search:
+                    print(registro)
+                    unidades_vendidas = input("- Ingrese unidades vendidas: ")
+                    registro['cantidad'] = str(int(registro['cantidad']) - int(unidades_vendidas))
+                    print(f"Nueva existencia de {registro['nombre']} ({registro['id']}) -> {registro['cantidad']} unidades")
+        continuar = int(input("Desea continuar descargando (1 > Si / 2 > No)? "))
+    
+    insert_producto(productos)
