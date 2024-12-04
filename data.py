31fde16f3e22478947c3927612b2fadc37a23e57
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-04 10:58:12 -0600

    save file names to variables, added evaluator of existence for path

--- data.py
+++ data.py
@@ -1,10 +1,19 @@
 import csv
+import os
+
+general_path = 'data/'
+productos_name = 'productos.csv'
+productos_file_path = os.path.join(general_path, productos_name)
+proveedores_name = 'proveedores.csv'
+proveedores_file_path = os.path.join(general_path, proveedores_name)
+
+os.makedirs(general_path, exist_ok=True)
 
 def insert_producto(productos):
 
     campos = ['id', 'nombre', 'cantidad', 'precio_entrada', 'precio_salida', 'proveedor']
 
-    with open('productos.csv', 'w', newline='') as productos_csv:
+    with open(productos_file_path, 'w', newline='') as productos_csv:
 
         producto_writer = csv.DictWriter(productos_csv, fieldnames=campos)
         producto_writer.writeheader()
@@ -14,11 +23,20 @@ def insert_producto(productos):
 
 def read_producto():
     productos = []
-    with open('productos.csv', newline='') as productos_csv:
+    with open(productos_file_path, newline='') as productos_csv:
         producto_reader = csv.DictReader(productos_csv, delimiter=',')
         for lines in producto_reader:
             productos.append(lines)
     
     return productos
 
+def insert_proveedor(proveedores):
+    campos = ['id', 'nombre', 'numero']
 
+    with open(proveedores_file_path, 'w', newline='') as proveedores_csv:
+
+        proveedor_writer = csv.DictWriter(proveedores_csv, fieldnames=campos)
+        proveedor_writer.writeheader()
+
+        for fila in proveedores:
+            proveedor_writer.writerow(fila)
