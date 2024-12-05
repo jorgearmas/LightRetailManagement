e55ea9b8a68a8cac743ae63eaa7622bbf971ec3d
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-05 13:11:47 -0600

    Modified: Set param as str istead of obj for class Producto

--- main.py
+++ main.py
@@ -39,27 +39,27 @@ while primera_ejecucion == 1:
             'cantidad': producto.cantidad, 
             'precio_entrada': producto.precio_entrada, 
             'precio_salida': producto.precio_salida, 
-            'proveedor': producto.proveedor.nombre})
+            'proveedor': proveedor.nombre})
 
         continuar = int(input("Desea agregar otro producto del mismo proveedor (1 > Si / 2 > No)? "))
     
     insert_producto(productos)
     primera_ejecucion = 2
 
-opcion_menu = 0
+# opcion_menu = 0
 
-print("1. Cargar inventario proveedor existente")
-print("2. Cargar inventario - solo stock")
-print("3. Cargar inventario proveedor nuevo")
-print("4. Descargar inventario")
-opcion_menu = int(input("Ingrese opcion: "))
+# print("1. Cargar inventario proveedor existente")
+# print("2. Cargar inventario - solo stock")
+# print("3. Cargar inventario proveedor nuevo")
+# print("4. Descargar inventario")
+# opcion_menu = int(input("Ingrese opcion: "))
 
-if opcion_menu == 1:
-    exec_carga_existente()
-elif opcion_menu == 2:
-    exec_carga_stock()
-elif opcion_menu == 3:
-    exec_carga_no_existente()
-elif opcion_menu == 4:
-    exec_descarga()
+# if opcion_menu == 1:
+#     exec_carga_existente()
+# elif opcion_menu == 2:
+#     exec_carga_stock()
+# elif opcion_menu == 3:
+#     exec_carga_no_existente()
+# elif opcion_menu == 4:
+#     exec_descarga()
     
