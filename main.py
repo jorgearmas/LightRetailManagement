63186396d4f4d3883b3f2d67a1fc8dfe4fb5f2e9
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-05 15:47:36 -0600

    Testing carga.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while primera_ejecucion == 1:
         precio_entrada = input("Precio de compra: ")
         precio_salida = input("Precio de venta: ")
 
-        producto = Producto(nombre_producto, cantidad, precio_entrada, precio_salida, proveedor)
+        producto = Producto(nombre_producto, cantidad, precio_entrada, precio_salida, proveedor.nombre)
         productos.append({
             'id': producto.id, 
             'nombre': producto.nombre, 
@@ -46,20 +46,20 @@ while primera_ejecucion == 1:
     insert_producto(productos)
     primera_ejecucion = 2
 
-# opcion_menu = 0
+opcion_menu = 0
 
-# print("1. Cargar inventario proveedor existente")
-# print("2. Cargar inventario - solo stock")
-# print("3. Cargar inventario proveedor nuevo")
-# print("4. Descargar inventario")
-# opcion_menu = int(input("Ingrese opcion: "))
+print("1. Cargar inventario proveedor existente")
+print("2. Cargar inventario - solo stock")
+print("3. Cargar inventario proveedor nuevo")
+print("4. Descargar inventario")
+opcion_menu = int(input("Ingrese opcion: "))
 
-# if opcion_menu == 1:
-#     exec_carga_existente()
-# elif opcion_menu == 2:
-#     exec_carga_stock()
-# elif opcion_menu == 3:
-#     exec_carga_no_existente()
+if opcion_menu == 1:
+    exec_carga_existente()
+elif opcion_menu == 2:
+    exec_carga_stock()
+elif opcion_menu == 3:
+    exec_carga_no_existente()
 # elif opcion_menu == 4:
 #     exec_descarga()
     
