25c74edc70df5953d8b9bfa5dc7ed53bf4ad579b
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-13 19:08:40 -0600

    All menu UI moved to menu module

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from proveedor import Proveedor
 from producto import Producto
 from data import insert_producto, insert_proveedor
-from carga import exec_carga_existente, exec_carga_no_existente, exec_carga_stock
+from menu import main_menu
 
-from descarga import *
-
-print("-- LIGHT RETAIL MANAGEMENT --")
 primera_ejecucion = int(input("Es esta la primera ejecucion (1 > Si / 2 > No)? "))
 
 while primera_ejecucion == 1:
@@ -46,20 +43,7 @@ while primera_ejecucion == 1:
     insert_producto(productos)
     primera_ejecucion = 2
 
-opcion_menu = 0
+main_menu()
 
-print("1. Cargar inventario proveedor existente")
-print("2. Cargar inventario - solo stock")
-print("3. Cargar inventario proveedor nuevo")
-print("4. Descargar inventario")
-opcion_menu = int(input("Ingrese opcion: "))
 
-if opcion_menu == 1:
-    exec_carga_existente()
-elif opcion_menu == 2:
-    exec_carga_stock()
-elif opcion_menu == 3:
-    exec_carga_no_existente()
-elif opcion_menu == 4:
-    exec_descarga()
-    
+        
