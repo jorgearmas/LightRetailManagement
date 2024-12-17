d72c3341d245abd5835f76be1db58d31b357cc97
Jorge Armas <jorgearmasc0496@gmail.com>
2024-12-16 20:50:54 -0600

    created dependency for 'descargas' menu

--- menu.py
+++ menu.py
@@ -1,11 +1,9 @@
 
 from carga import exec_carga_existente, exec_carga_no_existente, exec_carga_stock
 from descarga import exec_descarga
+import os
 
-opcion_menu = 0
-
-
-def main_menu():
+def main_menu(opcion_menu):
     print("******** LIGHT RETAIL MANAGEMENT ********")
     print("-----------------------------------------")
     print("1. Cargar inventario proveedor existente")
@@ -24,9 +22,31 @@ def main_menu():
         elif opcion_menu == 3:
             exec_carga_no_existente()
         elif opcion_menu == 4:
-            exec_descarga()
+            os.system('cls')
+            menu_descarga(0)
         elif opcion_menu == 5:
             exit()
         else:
             print("Ingrese opción correcta")
             opcion_menu = int(input("Ingrese opcion: "))
+
+def menu_descarga(opcion_menu_descarga):
+    print("******** LIGHT RETAIL MANAGEMENT ********")
+    print("********* Control de descargas **********")
+    print("1. Descargar por ID")
+    print("2. Seleccionar de inventario")
+    print("3. Regresar")
+    print("-----------------------------------------")
+    opcion_menu_descarga = int(input("Ingrese opcion: "))
+
+    while opcion_menu_descarga != 4:
+        if opcion_menu_descarga == 1:
+            exec_descarga()
+        elif opcion_menu_descarga == 2:
+            pass
+        elif opcion_menu_descarga == 3:
+            os.system('cls')
+            main_menu(0)
+        else:
+            print("Ingrese opción correcta")
+            opcion_menu_descarga = int(input("Ingrese opcion: "))
